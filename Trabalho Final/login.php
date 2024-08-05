b41eabc132fa441f1be3d1669368749b12cca839
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2024-08-05 16:27:55 -0300

    Telas trabalho final

---
+++ Trabalho Final/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div id="infos">
+        <h3>Insira os dados:</h3>
+        <label for="login">Login:</label>
+        <input type="text" id="login" name="login" class="bolhas">
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" class="bolhas">
+        <input type="submit" value="Entrar" id="btn">
+        <h5>Caso não tenha cadastro, clique abaixo:</h5>
+        <input type="submit" value="Cadastrar" id="btnCad">
+    </div>
+</body>
+</html>
---
+++ Trabalho Final/telaEdicao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div id="infos">
+        <h3>Insira as novas informações:</h3>
+        <label for="login">Login novo:</label>
+        <input type="text" id="login" name="login" class="bolhas">
+        <label for="senha">Senha nova:</label>
+        <input type="password" id="senha" name="senha" class="bolhas">
+        <input type="submit" value="Editar" id="btn">
+    </div>
+</body>
+</html>
